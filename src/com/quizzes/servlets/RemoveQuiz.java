c1dfc868918a26b42ad3ede08c3da167fe79670a
christky <christky@stanford.edu>
2015-03-07 12:28:36 -0800

    Initialization of Remove and TakeQuiz Servlet

--- src/com/quizzes/servlets/RemoveQuiz.java
+++ src/com/quizzes/servlets/RemoveQuiz.java
@@ -6,7 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import com.quizzes.*;
 /**
  * Servlet implementation class RemoveQuiz
  */
@@ -33,7 +33,6 @@ public class RemoveQuiz extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 }
