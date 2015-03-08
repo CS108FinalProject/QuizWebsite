097985fe1cf7014b2f99c548a117acc87250035a
gamdur <gamdur@stanford.edu>
2015-03-07 18:19:52 -0800

    Finished CreateQuiz.java

--- src/com/quizzes/servlets/CreateQuiz.java
+++ src/com/quizzes/servlets/CreateQuiz.java
@@ -1,7 +1,12 @@
 package com.quizzes.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,16 +17,23 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.accounts.Account;
 import com.accounts.AccountManager;
+import com.quizzes.FillBlank;
+import com.quizzes.Matching;
+import com.quizzes.MultiResponse;
+import com.quizzes.MultipleChoice;
+import com.quizzes.Picture;
 import com.quizzes.Question;
 import com.quizzes.Quiz;
 import com.quizzes.QuizManager;
+import com.quizzes.Response;
 import com.util.Json;
+import com.util.Util;
 
 /**
  * Servlet implementation class CreateQuiz
  */
 @WebServlet("/CreateQuiz")
-public class CreateQuiz extends HttpServlet {
+public class CreateQuiz extends HttpServlet implements com.util.Constants {
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -44,13 +56,19 @@ public class CreateQuiz extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// get quiz Json
-		String quiz_json = request.getParameter("quiz");
-		Map<String, Object> quiz_map = Json.parseJsonObject(quiz_json);
+		String dataString = (String)request.getParameter("json");
+		Map<String, Object> dataMap = Json.parseJsonObject(dataString);
 		
 		// create quiz
-		Map<String, Object> quizMetadata = (Map<String, Object>) quiz_map.get("quizMetadata");
+		Map<String, Object> quizMetadata  =  (Map<String, Object>)dataMap.get("quizMetaData");
 		String name = (String) quizMetadata.get("name");
-		Account creator = AccountManager.getAccount((String) quizMetadata.get("creator"));
+		Account creator = null;
+		if (AccountManager.accountExists(name)) {
+			creator = AccountManager.getAccount((String) quizMetadata.get("creator")); 
+			Util.addStatus(true,"", dataMap);
+		} else {
+			Util.addStatus(false,"There was no account found named, " + name, dataMap);
+		}
 		String description =  (String) quizMetadata.get("description");
 		String date = (String) quizMetadata.get("date");
 		boolean isRandom = (Boolean) quizMetadata.get("isRandom");
@@ -59,18 +77,46 @@ public class CreateQuiz extends HttpServlet {
 		Quiz quiz = QuizManager.createQuiz(name,creator,description,date,isRandom,isOnePage,isImmediate);
 		
 		// add questions
-		Map<String, Object>[] questions = (Map<String, Object>[]) quiz_map.get("questions");
+		List<Map<String, Object>> questions = (List<Map<String, Object>>) dataMap.get("questions");
 		for (Map<String, Object> question_map : questions) {
 			String type = (String) question_map.get("type");
-			Question question = (Question) question_map.get("question");
-			String[] answers = (String[]) question_map.get("answers");
+			Question question = null;
+			if (type.equals(FILL_BLANK)) {
+				question = (FillBlank) question_map.get("question");
+				Map<String, Set<String>> blanksAndAnswers = (Map<String, Set<String>>) question_map.get("answers");
+				((FillBlank) question).setBlanksAndAnswers(blanksAndAnswers);
+			} else if (type.equals(MULTIPLE_CHOICE)) {
+				question = (MultipleChoice) question_map.get("question");
+				Map<String, Boolean> options = (Map<String, Boolean>) question_map.get("answers");
+				((MultipleChoice) question).setOptions(options);
+			} else if (type.equals(PICTURE)) {
+				question = (Picture) question_map.get("question");
+				Set<String> answers = (Set<String>) question_map.get("answers");
+				((Picture) question).setAnswers(answers);
+			} else if (type.equals(MULTI_RESPONSE)) {
+				question = (MultiResponse) question_map.get("question");
+				TreeMap<Integer, String> answers = (TreeMap<Integer, String>) question_map.get("answers");
+				((MultiResponse) question).setAnswers(answers);
+			} else if (type.equals(MATCHING)) {
+				question = (Matching) question_map.get("question");
+				Map<String, String> matches = (Map<String, String>) question_map.get("answers");
+				((Matching) question).setMatches(matches);
+			} else if (type.equals(RESPONSE)) {
+				question = (Response) question_map.get("question");
+				Set<String> answers = ((Response) question).getAnswers();
+				((Response) question).setAnswers(answers);
+			}		
 			quiz.addQuestion(question);
 		}
 		
-		// forward request
-		request.setAttribute("quiz", quiz);
-		//RequestDispatcher dispatch = request.getRequestDispatcher("");
-		//dispatch.forward(request, response);
+		// turn the response_map into a Json string
+		String response_str = Json.getJsonString(dataMap);
+		
+		// give back to method which invoked this servlet
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		out.write(response_str);
+
 	}
 
 }
