4882e0a4d17218349825cd8bef0936800fa0c10c
samulegp <samuelg22@gmail.com>
2015-03-11 18:49:34 -0700

    Commented out unnessesary code.

--- src/com/quizzes/QuizManager.java
+++ src/com/quizzes/QuizManager.java
@@ -301,9 +301,9 @@ public class QuizManager implements Constants {
 	
 	
 	public static void main(String[] args) {
-		new Database();
-		Quiz quiz = QuizManager.getQuiz("TestIII");
-		System.out.println(quiz.getDescription());
+//		new Database();
+//		Quiz quiz = QuizManager.getQuiz("TestIII");
+//		System.out.println(quiz.getDescription());
 	}
 
 }
