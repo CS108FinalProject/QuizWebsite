1e23e878013748e15f5e934ad43e6c95a102f938
EliezerAbate <eliezer@stanford.edu>
2015-03-11 20:09:28 -0700

    rerouting

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -192,7 +192,6 @@
             event.preventDefault();
             console.log( "I am checked");
             var URL = "/QuizWebsite/EditQuizInit";
-            $('#lp-quiz-edit').prop("disabled",true);
             var quizName = $('#lp-quiz-name').text();
             $.ajax({
                 url: URL,
@@ -202,14 +201,14 @@
                     data: { quiz_name: quizName },
                     contentType: 'application/x-www-form-urlencoded',
 
-                    success: function(data, textStatus, jqXHR) {
+                    success: function(data) {
                         $('#my-quizzes').prop("disabled",false);
                         console.log( data );
-                        storeMyQuizzes(data);
-                        displayOnLeftPane(data);
+
+                        $('#right-pane').html( data );
                     }
             });
-            $('#right-pane').html( "HELLO" );
+            window.location.href='http://localhost:8080/QuizWebsite/editQuiz.jsp';
  
 
         });
@@ -239,6 +238,7 @@
                         displayOnLeftPane(data);
                     }
             }); 
+
         });
 
 
@@ -945,36 +945,36 @@
      /*************************TAKE SINGLE PAGE QUIZ LOGIC****************************/
 
      // request database for the quiz
-     var quizName = getUrlVar("quiz"); // tested and works
-     if ( quizName != "") {
-            var URL = "/QuizWebsite/GetData";
-            var request = {request: { type: "quiz", quiz_name: quizName}} // tested works
-            $.ajax({
-                   url: URL,
-                   type: 'POST',
-                   async: true,
-                   dataType: 'json',
-                   data: { json: JSON.stringify(request) },
-                   contentType: 'application/x-www-form-urlencoded',
+     // var quizName = getUrlVar("quiz"); // tested and works
+     // if ( quizName != "") {
+     //        var URL = "/QuizWebsite/GetData";
+     //        var request = {request: { type: "quiz", quiz_name: quizName}} // tested works
+     //        $.ajax({
+     //               url: URL,
+     //               type: 'POST',
+     //               async: true,
+     //               dataType: 'json',
+     //               data: { json: JSON.stringify(request) },
+     //               contentType: 'application/x-www-form-urlencoded',
 
-                   success: function(response, textStatus, jqXHR) {
-                       if ( response.status.success ) {
-                           storeBooleanTypeForQuestion( response );
-                       }
-                       // tested -> works proper way to obtain booleans
-                       console.log( getQuizToTake() );
-                       var onePage = getQuizToTake().quizMetaData.is_one_page;
-                       var isImmediate = getQuizToTake().quizMetaData.is_immediate;
-                       if ( onePage ) {           // checked works
-                           generateOnePageQuiz();
-                       } else {
-                           // test all values
-                           generateMultiplePageQuiz(isImmediate);
-                       }
+     //               success: function(response, textStatus, jqXHR) {
+     //                   if ( response.status.success ) {
+     //                       storeBooleanTypeForQuestion( response );
+     //                   }
+     //                   // tested -> works proper way to obtain booleans
+     //                   console.log( getQuizToTake() );
+     //                   var onePage = getQuizToTake().quizMetaData.is_one_page;
+     //                   var isImmediate = getQuizToTake().quizMetaData.is_immediate;
+     //                   if ( onePage ) {           // checked works
+     //                       generateOnePageQuiz();
+     //                   } else {
+     //                       // test all values
+     //                       generateMultiplePageQuiz(isImmediate);
+     //                   }
 
-                   }
-            }); 
-       }
+     //               }
+     //        }); 
+     //   }
 
 
     
